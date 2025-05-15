9e9d011739858f84b67ada2ddbb86acef2b1d444
IO <ivo.offenthaler@gmail.com>
2025-05-15 13:36:12 +0200

    add dist directory

---
+++ dist/_npm/lit-html@3.2.1/directives/when.js.777bf979.js
@@ -0,0 +1,12 @@
+/**
+ * Bundled by jsDelivr using Rollup v2.79.1 and Terser v5.19.2.
+ * Original file: /npm/lit-html@3.2.1/directives/when.js
+ *
+ * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
+ */
+/**
+ * @license
+ * Copyright 2021 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+function e(e,n,t){return e?n(e):t?.(e)}export{e as when};export default null;
