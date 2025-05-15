51ff80517d1bb025cc4ff1d7038f897114238f17
IO <ivo.offenthaler@gmail.com>
2025-05-15 13:36:12 +0200

    add dist directory

---
+++ _npm/lit-html@3.2.1/directives/map.js.e28c73b2.js
@@ -0,0 +1,12 @@
+/**
+ * Bundled by jsDelivr using Rollup v2.79.1 and Terser v5.19.2.
+ * Original file: /npm/lit-html@3.2.1/directives/map.js
+ *
+ * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
+ */
+/**
+ * @license
+ * Copyright 2021 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+function*o(o,t){if(void 0!==o){let e=0;for(const f of o)yield t(f,e++)}}export{o as map};export default null;
